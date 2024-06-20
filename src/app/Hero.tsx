f46ecebad45067255a6929bc973febb6af5d455a
Miky-131 <cryptogame131@gmail.com>
2024-06-21 07:01:20 +1000

    add link to button

--- src/app/Hero.tsx
+++ src/app/Hero.tsx
@@ -77,34 +77,44 @@ export default function Hero() {
       </div>
       <div className={`${styles.buttonContainer} flex flex-row`}>
         <Button className={`${styles.button} ${styles.buttonShadow}`}>
-          <div className="flex ml-[32px]">
-            <img src="/telegram-icon.svg" width="32px" className="mr-2" />
-            Telegram
-          </div>
+          <Link href="https://t.me/moonmouseannounce" target="_blank">
+            <div className="flex ml-[32px]">
+              <img src="/telegram-icon.svg" width="32px" className="mr-2" />
+              Telegram
+            </div>
+          </Link>
         </Button>
         <Button className={`${styles.button} ${styles.buttonShadow}`}>
-          <div className="flex ml-[42px]">
-            <img src="/x-icon.svg" width="32px" className="mr-2" />
-            Twitter
-          </div>
+          <Link href="https://x.com/moonmouse24" target="_blank">
+            <div className="flex ml-[42px]">
+              <img src="/x-icon.svg" width="32px" className="mr-2" />
+              Twitter
+            </div>
+          </Link>
         </Button>
         <Button className={`${styles.button} ${styles.buttonShadow}`}>
-          <div className="flex ml-[60px]">
-            <img src="/pinksale-icon.svg" width="32px" className="mr-2" />
-            Buy
-          </div>
+          <Link href="https://www.pinksale.finance/" target="_blank">
+            <div className="flex ml-[60px]">
+              <img src="/pinksale-icon.svg" width="32px" className="mr-2" />
+              Buy
+            </div>
+          </Link>
         </Button>
         <Button className={`${styles.button} ${styles.buttonShadow}`}>
-          <div className="flex ml-[44px]">
-            <img src="/cfgninja-icon.png" width="32px" className="mr-2" />
-            Audit
-          </div>
+          <Link href="https://audit.cfg.ninja/" target="_blank">
+            <div className="flex ml-[44px]">
+              <img src="/cfgninja-icon.png" width="32px" className="mr-2" />
+              Audit
+            </div>
+          </Link>
         </Button>
         <Button className={`${styles.button} ${styles.buttonShadow}`}>
-          <div className="flex ml-[22px]">
-            <img src="/pdf-icon.svg" width="32px" className="mr-1" />
-            Whitepaper
-          </div>
+          <Link href="https://docs.moonmouse.io/" target="_blank">
+            <div className="flex ml-[22px]">
+              <img src="/pdf-icon.svg" width="32px" className="mr-1" />
+              Whitepaper
+            </div>
+          </Link>
         </Button>
       </div>
     </div>
